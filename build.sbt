5f2fdb10658ef5c773057709e8d9f33bd3ab0deb
Ryoji Kodakari <ryoji.kodakari@gmail.com>
2017-05-13 12:47:52 +0100

    initial commit

---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := "HackTheTower"
+
+version := "1.0"
+
+scalaVersion := "2.12.2"
+
+//libraryDependencies += scalaTest % Test
+
+//libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.6" % "test"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.0" % Test
---
+++ src/main/scala/FizzBuzz.scala
@@ -0,0 +1,14 @@
+/**
+  * Created by ryoji on 13/05/17.
+  */
+object FizzBuzz {
+
+  def getResult(number: Int) = {
+    var result = ""
+    if (number % 3 == 0) result = "fizz"
+    if (number % 5 == 0) result += "buzz"
+    if (result == "") number.toString
+    else result
+  }
+
+}
