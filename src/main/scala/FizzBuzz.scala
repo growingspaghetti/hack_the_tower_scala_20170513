9ff85b2c8ceaec58ce04e92a58855e628f936004
Ryoji Kodakari <ryoji.kodakari@gmail.com>
2017-05-13 13:55:36 +0100

    with Matchers

--- src/main/scala/FizzBuzz.scala
+++ src/main/scala/FizzBuzz.scala
@@ -5,7 +5,7 @@ object FizzBuzz {
 
   def getResult(number: Int) = {
     var result = ""
-    if (number % 3 == 0) result = "fizz"
+    if (number % 3 == 0) result = "fizjjz"
     if (number % 5 == 0) result += "buzz"
     if (result == "") number.toString
     else result
--- src/test/scala/FizzBuzzTest.scala
+++ src/test/scala/FizzBuzzTest.scala
@@ -1,11 +1,11 @@
 import org.scalatest._
+
 /**
   * Created by ryoji on 13/05/17.
   */
-class FizzBuzzTest extends FlatSpec {
-
+class FizzBuzzTest extends FlatSpec with Matchers {
     "FizzBuzz" should "return fizz if the number is dividable by 3" in {
-      FizzBuzz.getResult(3) eq ("fizz")
-      FizzBuzz.getResult(6) eq ("fizz")
+      FizzBuzz.getResult(3) should be ("fizz")
+      FizzBuzz.getResult(6) should be ("fizz")
     }
   }
